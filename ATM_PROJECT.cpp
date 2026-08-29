1fbaf052a239fc547acf96c8ce0f809e4845086d
Polkandot <jusmemarimar@gmail.com>
2026-08-29 13:15:30 +0800

    Implement Account struct and Module class for ATM
    Added Account struct and Module class with methods for ATM functionalities.

--- ATM_PROJECT.cpp
+++ ATM_PROJECT.cpp
@@ -1,11 +1,70 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
+
+const string fn="database.csv";
+
+struct Account{
+    int AccNo, InitialDeposit;
+    string Name, Birthday, Contact, Pin code;
+};
+
+class Module{
+    private:
+        void encryption();
+        void save();
+        void retrieve();
+    public:
+        void Enrollment();
+        void Balcheck();
+        void Withdraw();
+        void Deposit();
+        void transfer();
+        void changePIN();
+};
+
+void Module :: Enrollment(){
+
+}
+
+void Module :: Balcheck(){
+
+}
+
+void Module :: Withdraw(){
+
+}
+
+void Module :: Deposit(){
+
+}
+
+void Module :: transfer(){
+
+}
+
+void Module :: changePIN(){
+
+}
+
+void Module :: encryption(){
+
+}
+
+void Module :: save(){
+
+}
+
+void Module :: retrieve(){
+
+}
 
 
 int main(){
-    cout << "hello world!" << endl;
-    cout << "Game na Sean, start na tayo!";
-
 
     return 0;
 }
